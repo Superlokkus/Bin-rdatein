0392a31959c6048fc31b115f7dd33ddf822dac27
Markus Klemm <markus@markusklemm.net>
2013-12-19 13:15:29 +0100

    Initial Commit

---
+++ Binärdateien/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Binärdateien
+//
+//  Created by Markus Klemm on 19.12.13.
+//  Copyright (c) 2013 Markus Klemm Enterprises. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
